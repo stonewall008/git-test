ad1a12a61fbb172efee3514dc5f23fc93d758eef
root <root@qq.com>
2022-08-04 13:56:06 +0800

    first commit

---
+++ src/main/java/study/git/GitTest.java
@@ -0,0 +1,11 @@
+package study.git;
+
+/**
+ * @author shkstart
+ * @create 2022-08-04 13:52
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
